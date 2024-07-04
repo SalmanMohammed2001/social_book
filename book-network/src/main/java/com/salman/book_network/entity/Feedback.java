fa3b9474b017c71ef8b4167d8eb0e3b77a8eff0e
salman <salmannisthar123@gmail.com>
2024-07-04 13:23:34 +0530

    create BookTransactionHistory entity

--- book-network/src/main/java/com/salman/book_network/entity/Feedback.java
+++ book-network/src/main/java/com/salman/book_network/entity/Feedback.java
@@ -5,13 +5,7 @@ import com.salman.book_network.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
